35c333bb5a4dd964973d15cd46dbe5c9d82c4ebb
Danila Garus <danilagarus2003@gmail.com>
2022-05-31 00:00:13 +0300

    ...

--- BulkyBookWeb (.NET)/BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb (.NET)/BulkyBookWeb/Models/Category.cs
@@ -7,9 +7,8 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
-    
-    [Required]
-    public string Name { get; set; }
+
+    [Required] public string Name { get; set; } = null!;
     
     [DisplayName("Display Order")]
     [Range(1,100,ErrorMessage = "Display Order must be between 1 and 100 only!")]
---
+++ Some Entity course/Some Entity course/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Some_Entity_course.Data;
+
+public class DataContext : DbContext
+{
+    
+}
