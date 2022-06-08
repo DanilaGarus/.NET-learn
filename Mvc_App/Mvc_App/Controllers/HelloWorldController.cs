545755ff141df588e64c7f228b9d3aac109c9c9d
Danila Garus <danilagarus2003@gmail.com>
2022-06-09 02:07:23 +0300

    ...

---
+++ Mvc_App/Mvc_App/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc_App.Models;
+
+namespace Mvc_App.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel dog = new DogViewModel() {Name = "Sif", Age = 2};
+            return View(dog);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVM = new DogViewModel();
+            return View(dogVM);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+           // return View("Index");
+           return RedirectToAction(nameof(Index));
+        }
+
+        public String Hello()
+        {
+            return "Whos there?";
+        }
+    }
+}
---
+++ Mvc_App/Mvc_App/Models/DogViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Mvc_App.Models;
+
+public class DogViewModel
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
