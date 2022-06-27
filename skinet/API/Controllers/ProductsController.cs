050c80c36b5a063208f91df9e7223730e280f17c
Danila Garus <danilagarus2003@gmail.com>
2022-06-27 23:12:19 +0300

    ...

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,45 +1,76 @@
-﻿using Core.Entities;
+﻿using API.Dtos;
+using API.Errors;
+using API.Helpers;
+using AutoMapper;
+using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : Controller
+public class ProductsController : BaseApiController
 {
-    private readonly IProductRepository _repository;
+    private readonly IGenericRepository<Products> _productsRepository;
+    private readonly IGenericRepository<ProductBrand> _productBrandRepository;
+    private readonly IGenericRepository<ProductType> _productTypeRepository;
+    private readonly IMapper _mapper;
 
-    public ProductsController(IProductRepository repository)
+    public ProductsController(
+        IGenericRepository<Products> productsRepository,
+        IGenericRepository<ProductBrand> productBrandRepository,
+        IGenericRepository<ProductType> productTypeRepository,
+        IMapper mapper)
     {
-        _repository = repository;
+        _productsRepository = productsRepository;
+        _productBrandRepository = productBrandRepository;
+        _productTypeRepository = productTypeRepository;
+        _mapper = mapper;
     }
-    
-    [HttpGet]
-    public async Task<ActionResult<List<Products>>> GetProducts()
-    {
-        var products = await _repository.GetProductsAsync();
 
-        return Ok(products);
+    [HttpGet]
+    public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+        [FromQuery]ProductsSpecParams productsParams)
+    {
+        var spec = new ProductsWithTypesAndBrandsSpecification(productsParams);
+
+        var countSpec = new ProductsWithFiltersForCountSpecification(productsParams);
+
+        var totalItems = await _productsRepository.CountAsync(countSpec);
+
+        var products = await _productsRepository.ListAsync(spec);
+
+        var data = _mapper
+            .Map<IReadOnlyList<Products>, IReadOnlyList<ProductToReturnDto>>(products);
+
+        return Ok(new Pagination<ProductToReturnDto>(productsParams.PageIndex, productsParams.PageSize, totalItems, data));
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Products>> GetProduct(int id)
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
     {
-        return await _repository.GetProductsByIdAsync(id);
+        var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+        var products = await _productsRepository.GetEntityWithSpec(spec);
+
+        if (products == null) return NotFound(new ApiResponse(404));
+
+        return _mapper.Map<Products, ProductToReturnDto>(products);
     }
 
     [HttpGet("brands")]
     public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
     {
-        return Ok(await _repository.GetProductBrandsAsync());
+        return Ok(await _productBrandRepository.ListAllAsync());
     }
-    
+
     [HttpGet("types")]
     public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
     {
-        return Ok(await _repository.GetProductTypesAsync());
+        return Ok(await _productTypeRepository.ListAllAsync());
     }
 }
--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -1,31 +1,47 @@
-using Core.Interfaces;
+using API.Extensions;
+using API.Helpers;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(x =>
     x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
+builder.Services.AddSwaggerDocumentation();
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
+
+app.UseSwaggerDocumentation();
 
 app.MapControllers();
 
